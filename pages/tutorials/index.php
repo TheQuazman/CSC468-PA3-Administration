6d5ca27251e8c99077feb7be4f4c440fbb2a67ce
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2016-04-13 09:41:42 -0600

    Started the side menu for the page. Also added a css file to accompany it.

--- pages/tutorials/index.php
+++ pages/tutorials/index.php
@@ -4,10 +4,45 @@
  */
 ?>
 <html>
-  <head>
-    <title>Alumnai</title>
-  </head>
-  <body>
-    Example page.
-  </body>
+    <head>
+        <title>Tutorials</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        Example page.
+
+    <div class="page">
+        <div class="maincontent">
+            <div class="firstcontent">Page Menu</div>
+        </div>
+    </div>
+
+    <div class="leftcontent">
+        <ul class="menu">
+            <li class="leftitem1"><a href="../alumni/index.php">Alumni</a></li>
+            <li class="leftitem2"><a href="../checklist/index.php">Checklist</a></li>
+            <li class="leftitem1"><a href="../courses/index.php">Courses</a></li>
+            <li class="leftitem2"><a href="../directory/index.php">Directory</a></li>
+            <li class="leftitem1"><a href="../faculty/index.php">Faculty</a></li>
+            <li class="leftitem2"><a href="../home/index.php">Courses</a></li>
+            <li class="leftitem1"><a href="../login/index.php">Login</a></li>
+            <li class="leftitem2"><a href="../map/index.php">Map</a></li>
+            <li class="leftitem1"><a href="../policy/index.php">Policy</a></li>
+            <li class="leftitem2"><a href="../program/index.php">Program</a></li>
+            <li class="leftitem1"><a href="../research/index.php">Research</a></li>
+            <li class="leftitem2"><a href="../scheduler/index.php">Scheduler</a></li>
+            <li class="leftitem1"><a href="../student-orgs/index.php">Student Organizations</a></li>
+            <li class="leftitem2"><a href="../students/index.php">Students</a></li>
+            <li class="leftitem1"><a href="../submit/index.php">Submit it!</a></li>
+            <li class="leftitem2"><a href="../test/index.php">Test</a></li>
+            <li class="currleftitem"><a href="../tutorials/index.php">Tutorials</a></li>
+
+
+
+        </ul>
+    </div>
+
+    <div class="clearingdiv"></div>
+
+    </body>
 </html>
