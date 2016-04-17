4e0e6b16aa29aabff13a0f6a5af3ffa7e90ffdad
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2016-04-17 13:05:45 -0600

    Cleaned up some of the html.

--- pages/tutorials/index.php
+++ pages/tutorials/index.php
@@ -7,41 +7,33 @@
     <head>
         <title>Tutorials</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <img class="header" src="header.png" />
     </head>
     <body>
 
-    <div class="page">
-        <div class="maincontent">
-            <div class="firstcontent">Page Menu</div>
-        </div>
-    </div>
 
-    <div class="leftcontent">
-        <ul class="menu">
-            <li class="leftitem1"><a href="../alumni/index.php">Alumni</a></li>
-            <li class="leftitem2"><a href="../checklist/index.php">Checklist</a></li>
-            <li class="leftitem1"><a href="../courses/index.php">Courses</a></li>
-            <li class="leftitem2"><a href="../directory/index.php">Directory</a></li>
-            <li class="leftitem1"><a href="../faculty/index.php">Faculty</a></li>
-            <li class="leftitem2"><a href="../home/index.php">Courses</a></li>
-            <li class="leftitem1"><a href="../login/index.php">Login</a></li>
-            <li class="leftitem2"><a href="../map/index.php">Map</a></li>
-            <li class="leftitem1"><a href="../policy/index.php">Policy</a></li>
-            <li class="leftitem2"><a href="../program/index.php">Program</a></li>
-            <li class="leftitem1"><a href="../research/index.php">Research</a></li>
-            <li class="leftitem2"><a href="../scheduler/index.php">Scheduler</a></li>
-            <li class="leftitem1"><a href="../student-orgs/index.php">Student Organizations</a></li>
-            <li class="leftitem2"><a href="../students/index.php">Students</a></li>
-            <li class="leftitem1"><a href="../submit/index.php">Submit it!</a></li>
-            <li class="leftitem2"><a href="../test/index.php">Test</a></li>
-            <li class="currleftitem"><a href="../tutorials/index.php">Tutorials</a></li>
-
-
-
-        </ul>
-    </div>
-
-    <div class="clearingdiv"></div>
+    <!-- Left menu tabs -->
+    <h2 class="leftmenuheader">Site Map</h2>
+    <!--<ul class="leftmenu" style="list-style: none;">-->
+    <ul class="leftmenu">
+        <li class="leftitem1"><a href="../alumni/index.php">Alumni</a></li>
+        <li class="leftitem2"><a href="../checklist/index.php">Checklist</a></li>
+        <li class="leftitem1"><a href="../courses/index.php">Courses</a></li>
+        <li class="leftitem2"><a href="../directory/index.php">Directory</a></li>
+        <li class="leftitem1"><a href="../faculty/index.php">Faculty</a></li>
+        <li class="leftitem2"><a href="../home/index.php">Courses</a></li>
+        <li class="leftitem1"><a href="../login/index.php">Login</a></li>
+        <li class="leftitem2"><a href="../map/index.php">Map</a></li>
+        <li class="leftitem1"><a href="../policy/index.php">Policy</a></li>
+        <li class="leftitem2"><a href="../program/index.php">Program</a></li>
+        <li class="leftitem1"><a href="../research/index.php">Research</a></li>
+        <li class="leftitem2"><a href="../scheduler/index.php">Scheduler</a></li>
+        <li class="leftitem1"><a href="../student-orgs/index.php">Student Organizations</a></li>
+        <li class="leftitem2"><a href="../students/index.php">Students</a></li>
+        <li class="leftitem1"><a href="../submit/index.php">Submit it!</a></li>
+        <li class="leftitem2"><a href="../test/index.php">Test</a></li>
+        <li class="currentleftitem"><a href="../tutorials/index.php">Tutorials</a></li>
+    </ul>
 
     </body>
 </html>
