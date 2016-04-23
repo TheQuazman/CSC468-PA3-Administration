02426986d8e4465caf3a0705d3d6d48ab99a6791
Joseph Mowry <joseph.mowry@mines.sdsmt.edu>
2016-04-23 16:06:05 -0600

    Changed various padding/margin attributes. Looking beautiful.

--- pages/tutorials/index.php
+++ pages/tutorials/index.php
@@ -12,6 +12,17 @@
     </head>
     <body>        
 
+<!-- #This section needs to be checking for certain permissions in order to display. -->
+<!-- Currently displays regardless of permissions. -->
+    <div class="adminButtons">
+    <p>Welcome, Admin!</p>
+        <button type="button" id="viewButton" onclick="alert('Hello world!')">View All Elements</button>
+        <button type="button" id="addButton" onclick="alert('Hello world!')">Add</button>
+        <button type="button" id="editButton" onclick="alert('Hello world!')">Edit</button>
+        <button type="button" id="deleteButton" onclick="alert('Hello world!')">Delete</button>
+    </div>
+<!-- # -->
+
         <div id="header">
             <img class="header-logo " src="SDSMT-Logo.jpg"/>
             Mathematics and Computer Science Department
@@ -24,25 +35,27 @@
             <!-- Left menu tabs -->
             <h2 class="leftmenuheader">Site Map</h2>
             <!--<ul class="leftmenu" style="list-style: none;">-->
-            <ul class="leftmenu">
-                <li class="leftitem1"><a href="../alumni/index.php">Alumni</a></li>
-                <li class="leftitem2"><a href="../checklist/index.php">Checklist</a></li>
-                <li class="leftitem1"><a href="../courses/index.php">Courses</a></li>
-                <li class="leftitem2"><a href="../directory/index.php">Directory</a></li>
-                <li class="leftitem1"><a href="../faculty/index.php">Faculty</a></li>
-                <li class="leftitem2"><a href="../home/index.php">Courses</a></li>
-                <li class="leftitem1"><a href="../login/index.php">Login</a></li>
-                <li class="leftitem2"><a href="../map/index.php">Map</a></li>
-                <li class="leftitem1"><a href="../policy/index.php">Policy</a></li>
-                <li class="leftitem2"><a href="../program/index.php">Program</a></li>
-                <li class="leftitem1"><a href="../research/index.php">Research</a></li>
-                <li class="leftitem2"><a href="../scheduler/index.php">Scheduler</a></li>
-                <li class="leftitem1"><a href="../student-orgs/index.php">Student Organizations</a></li>
-                <li class="leftitem2"><a href="../students/index.php">Students</a></li>
-                <li class="leftitem1"><a href="../submit/index.php">Submit it!</a></li>
-                <li class="leftitem2"><a href="../test/index.php">Test</a></li>
-                <li class="currentleftitem"><a href="../tutorials/index.php">Tutorials</a></li>
-            </ul>
+            <div class="list-container">
+                <ul class="leftmenu">
+                    <li class="leftitem1"><a href="../alumni/index.php">Alumni</a></li>
+                    <li class="leftitem2"><a href="../checklist/index.php">Checklist</a></li>
+                    <li class="leftitem1"><a href="../courses/index.php">Courses</a></li>
+                    <li class="leftitem2"><a href="../directory/index.php">Directory</a></li>
+                    <li class="leftitem1"><a href="../faculty/index.php">Faculty</a></li>
+                    <li class="leftitem2"><a href="../home/index.php">Courses</a></li>
+                    <li class="leftitem1"><a href="../login/index.php">Login</a></li>
+                    <li class="leftitem2"><a href="../map/index.php">Map</a></li>
+                    <li class="leftitem1"><a href="../policy/index.php">Policy</a></li>
+                    <li class="leftitem2"><a href="../program/index.php">Program</a></li>
+                    <li class="leftitem1"><a href="../research/index.php">Research</a></li>
+                    <li class="leftitem2"><a href="../scheduler/index.php">Scheduler</a></li>
+                    <li class="leftitem1"><a href="../student-orgs/index.php">Student Organizations</a></li>
+                    <li class="leftitem2"><a href="../students/index.php">Students</a></li>
+                    <li class="leftitem1"><a href="../submit/index.php">Submit it!</a></li>
+                    <li class="leftitem2"><a href="../test/index.php">Test</a></li>
+                    <li class="currentleftitem"><a href="../tutorials/index.php">Tutorials</a></li>
+                </ul>
+            </div>
         </div>
 
   	    <div class="body-container">
@@ -54,7 +67,7 @@
                     </div>
 			    </details>
 			    <details open>
-				    <summary style="font-size:30px;">Tools</summary>
+    				    <summary style="font-size:30px;">Tools</summary>
                     <div style="padding-left:30px; background: inherit;">
 				        <?php displayXML('tools.xml');?>
                     </div>
