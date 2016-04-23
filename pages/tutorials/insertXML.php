d9dee4defde7f5212958a9c8eef80ad0aef9a6d5
Quaz <rquasney@live.com>
2016-04-23 15:52:14 -0600

    Adding insertion into the header for Location to prevent refresh causing a resending of the form.

--- pages/tutorials/insertXML.php
+++ pages/tutorials/insertXML.php
@@ -22,12 +22,30 @@
 			$link = "http://" . $link;
 		}
 
+		$attrs = array();
+		$nodes = $xml->xpath('/*/item[last()]');
+		if (is_array($nodes) && ! empty($nodes))
+		{
+    		foreach ($nodes[0]->attributes() as $attr)
+			{
+        		$attrs[$attr->getName()] = (string) $attr;
+    		}
+		}
+		
+		$id = $attrs['id'] + 1;
+
         $item = $xml->addChild(item);
+		$item->addAttribute('id', (string)$id);
         $item->addAttribute('name', $_POST['name']);
         $item->addAttribute('link', $link);
         $item->addAttribute('description', $_POST['description']);
 
         $xml->asXML($fileName);
+
+	$previous = "javascript:history.go(-1)";
+    	if(isset($_SERVER['HTTP_REFERER']))
+        	$previous = $_SERVER['HTTP_REFERER'];
+    	header("Location:$previous");
     }
 ?>
 
